8f203b2ca20255ce5dc7b1f27e3aa9f29cd77464
dmitriy.grytsovets <dmitriy.grytsovets@odnoklassniki.ru>
2015-12-11 14:09:20 +0300

    2.0.3

--- OKSDK.m
+++ OKSDK.m
@@ -3,7 +3,7 @@
 #import <UIKit/UIKit.h>
 #import "OKSDK.h"
 
-NSString *const OK_SDK_VERSION = @"2.0.1";
+NSString *const OK_SDK_VERSION = @"2.0.3";
 NSTimeInterval const OK_REQUEST_TIMEOUT = 180.0;
 NSInteger const OK_MAX_CONCURRENT_REQUESTS = 3;
 NSString *const OK_OAUTH_URL = @"https://connect.ok.ru/oauth/authorize";
@@ -462,16 +462,6 @@ static OKConnection *connection;
 +(void)clearAuth {
     [connection clearAuth];
 }
-
-+ (NSString*) currentAccessToken{
-    if (connection){
-        return connection.accessToken;
-    }else{
-        return nil;
-    }
-}
-
-@end
 
 + (NSString*) currentAccessToken{
     if (connection){
