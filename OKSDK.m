ca68e15b932841df11fe791f10ddf05e8d72e34a
Max Ovtsin <m.ovtsin@topface.com>
2015-12-11 13:50:47 +0300

    fix

--- OKSDK.m
+++ OKSDK.m
@@ -3,7 +3,7 @@
 #import <UIKit/UIKit.h>
 #import "OKSDK.h"
 
-NSString *const OK_SDK_VERSION = @"2.0.0";
+NSString *const OK_SDK_VERSION = @"2.0.1";
 NSTimeInterval const OK_REQUEST_TIMEOUT = 180.0;
 NSInteger const OK_MAX_CONCURRENT_REQUESTS = 3;
 NSString *const OK_OAUTH_URL = @"https://connect.ok.ru/oauth/authorize";
@@ -210,7 +210,6 @@ typedef void (^OKCompletitionHander)(id data, NSError *error);
 }
 
 -(void)cancel {
-    __weak typeof(self) wSelf = self;
     dispatch_async(dispatch_get_main_queue(), ^{
         [self dismissViewControllerAnimated:true completion:nil];
     });
